8f259204ca84015f650ffb0b3c6a232840b2a245
Rushil Vikram Borad <rs519505@dal.ca>
2024-07-12 21:44:24 -0300

    test changes

--- app.js
+++ app.js
@@ -61,8 +61,9 @@ app.post('/calculate', async (req, res) => {
 
     try {
         console.log("Sending request to Container 2...");
+        // const response = await axios.post('http://localhost:2000/calculate', { file, product });
         const response = await axios.post('http://container2-service:90/calculate', { file, product });
-        console.log("Received response from Container 2", response.data);
+        console.log("Received response from Container 2:", response.data);
 
         return res.json(response.data); // Return the response from container 2
     } catch (error) {
